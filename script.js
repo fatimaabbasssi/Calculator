8ef275f7cfe54ce32cb4c14d51c05e8b45c7efb1
fatimaabbasssi <artistfatimaabbasi@gmail.com>
2024-11-06 21:11:51 +0500

    js-calculator

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ function toclear(input){
     display.value = "" 
 }
 
-// function toclear2(input){
-//     display.value =  input
-// }
+function toclear2(input){
+ 
+    display.value = display.value.slice(0,-1)
+    
+}
 
 function toclick(input){
     display.value += input
 }
 
 
-function tocalcu(input){
+function tocalculate(input){
     display.value =  eval(display.value)
 }
